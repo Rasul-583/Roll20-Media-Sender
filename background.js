411a7ba910d88438bf50f8170da6568e1c9f80c1
Rasul-583 <rasul.abdullayev583@gmail.com>
2023-03-01 13:02:40 +0100

    reverted to old background.js

--- background.js
+++ background.js
@@ -1,26 +1,31 @@
+chrome.runtime.onMessage.addListener( data => {
+  if ( data.type === 'notification' ) {
+    notify( data.message );
+  }
+});
+
 chrome.runtime.onInstalled.addListener( () => {
-    chrome.contextMenus.create({
-      id: 'sender',
-      title: "test title", 
-      contexts:[ "selection", "link", "image", "video" ]
-    });
+  chrome.contextMenus.create({
+    id: 'notify',
+    title: "Notify!: %s", 
+    contexts:[ "selection" ]
   });
-  
-  chrome.contextMenus.onClicked.addListener( async ( info, tab ) => {
-    if ( 'sender' === info.menuItemId ) {
-      try {
-        const imgURL = '/images/generic/file.png';
-        const data = await fetch(imgURL);
-        const blob = await data.blob();
-        await navigator.clipboard.write([
-          new ClipboardItem({
-            [blob.type]: blob
-          })
-        ]);
-        console.log('Image copied.');
-      } catch (err) {
-        console.error(err.name, err.message);
-      }
+});
+
+chrome.contextMenus.onClicked.addListener( ( info, tab ) => {
+  if ( 'notify' === info.menuItemId ) {
+    notify( info.selectionText );
+  }
+} );
+
+const notify = message => {
+  return chrome.notifications.create(
+    '',
+    {
+      type: 'basic',
+      title: 'Notify!',
+      message: message || 'Notify!',
+      iconUrl: './assets/icons/128.png',
     }
-  } );
-  
+  );
+};
